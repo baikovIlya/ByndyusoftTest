95e27a834f5072ce35155a2a3805f3fabced5993
baikovIlya <baikusha23@gmail.com>
2022-10-21 01:35:18 +0300

    add: waiting for ButtonOrderPresentation

--- ByndyusoftTest/Support/PageObjects/ByndyusoftMainPage.cs
+++ ByndyusoftTest/Support/PageObjects/ByndyusoftMainPage.cs
@@ -2,7 +2,7 @@
 {
     public class ByndyusoftMainPage : AbstractPage
     {
-        public IWebElement ButtonOrderPresentation => _driver.FindElement(By.XPath("//*[text()='Узнайте больше о наших продуктах']/following-sibling::*[contains(text(),'Заказать презентацию')]"));
+        public IWebElement ButtonOrderPresentation => GetButtonOrderPresentation();
         public IWebElement LinkMailInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='mailto:sales@byndyusoft.com']"));
         public IWebElement LinkPhoneInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='tel:88007751521']"));
         public IWebElement PopUpWindowOrderPresentation => _driver.FindElement(By.XPath("//div[contains(@class,'popup-callback js')]"));
@@ -14,6 +14,12 @@
         public void ClickButtonOrderPresentation()
         {
             ButtonOrderPresentation.Click();
+        }
+
+        private IWebElement GetButtonOrderPresentation()
+        {
+            WebDriverWait webDriverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
+            return webDriverWait.Until(ExpectedConditions.ElementExists(By.XPath($"//*[text()='Узнайте больше о наших продуктах']/following-sibling::*[contains(text(),'Заказать презентацию')]")));
         }
     }
 }
