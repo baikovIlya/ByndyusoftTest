edb9494a6ae4b87631b983e0eb1c7976c8e2a8f1
baikovIlya <baikusha23@gmail.com>
2022-10-21 00:34:20 +0300

    add: PopUpWindowOrderPresentation to ByndyusoftMainPage

--- ByndyusoftTest/Support/PageObjects/ByndyusoftMainPage.cs
+++ ByndyusoftTest/Support/PageObjects/ByndyusoftMainPage.cs
@@ -5,6 +5,7 @@
         public IWebElement ButtonOrderPresentation => _driver.FindElement(By.XPath("//*[text()='Узнайте больше о наших продуктах']/following-sibling::*[contains(text(),'Заказать презентацию')]"));
         public IWebElement LinkMailInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='mailto:sales@byndyusoft.com']"));
         public IWebElement LinkPhoneInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='tel:88007751521']"));
+        public IWebElement PopUpWindowOrderPresentation => _driver.FindElement(By.XPath("//div[contains(@class,'popup-callback js')]"));
 
         public ByndyusoftMainPage(string driver) : base(driver)
         {
