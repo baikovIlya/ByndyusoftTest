778f1071370b30ffd8e9908a48cd31ba8e795a74
baikovIlya <baikusha23@gmail.com>
2022-10-21 01:02:38 +0300

    fix: define steps

--- ByndyusoftTest/StepDefinitions/PopUpWindowOrderPresentationStepDefinitions.cs
+++ ByndyusoftTest/StepDefinitions/PopUpWindowOrderPresentationStepDefinitions.cs
@@ -6,49 +6,61 @@ namespace ByndyusoftTest.StepDefinitions
         [Given(@"Open Google web page")]
         public void GivenOpenGoogleWebPage()
         {
-            throw new PendingStepException();
+            var page = new GoogleMainPage();
+            page.OpenThisPage();
         }
 
         [When(@"Input ""([^""]*)"" in textbox search")]
-        public void WhenInputInTextboxSearch(string byndysoft)
+        public void WhenInputInTextboxSearch(string searchText)
         {
-            throw new PendingStepException();
+            var page = new GoogleMainPage();
+            page.InputSearchText(searchText);
         }
 
         [When(@"Click button Search In Google")]
         public void WhenClickButtonSearchInGoogle()
         {
-            throw new PendingStepException();
+            var page = new GoogleMainPage();
+            page.ClickButtonSearchInGoogle();
         }
 
         [When(@"Click on first link")]
         public void WhenClickOnFirstLink()
         {
-            throw new PendingStepException();
+            var page = new GoogleSearchPage();
+            page.ClickFirstSearchResult();
         }
 
         [When(@"Click on button OrderPresentation")]
         public void WhenClickOnButtonOrderPresentation()
         {
-            throw new PendingStepException();
+            var page = new ByndyusoftMainPage();
+            page.ClickButtonOrderPresentation();
         }
 
         [Then(@"PopUp window should appear")]
         public void ThenPopUpWindowShouldAppear()
         {
-            throw new PendingStepException();
+            var page = new ByndyusoftMainPage();
+            Assert.True(page.PopUpWindowOrderPresentation.Displayed);
         }
 
         [Then(@"Email in PopUp window should match ""([^""]*)""")]
-        public void ThenEmailInPopUpWindowShouldMatch(string p0)
+        public void ThenEmailInPopUpWindowShouldMatch(string email)
         {
-            throw new PendingStepException();
+            var page = new ByndyusoftMainPage();
+            string expectedEmail = email;
+            string actualEmail = page.LinkMailInPopUpWindow.Text;
+            Assert.Equal(expectedEmail, actualEmail);
         }
 
         [Then(@"Phone number in PopUp window should match ""([^""]*)""")]
-        public void ThenPhoneNumberInPopUpWindowShouldMatch(string p0)
+        public void ThenPhoneNumberInPopUpWindowShouldMatch(string phone)
         {
-            throw new PendingStepException();
+            var page = new ByndyusoftMainPage();
+            string expectedPhone = phone;
+            string actualPhone = page.LinkPhoneInPopUpWindow.Text;
+            Assert.Equal(expectedPhone, actualPhone);
         }
     }
 }
