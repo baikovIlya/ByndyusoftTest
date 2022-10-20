2d0485b1b13e19682695395d2d903e091e5bf96b
baikovIlya <baikusha23@gmail.com>
2022-10-20 23:36:37 +0300

    add: Google pages

---
+++ ByndyusoftTest/Support/PageObjects/GoogleMainPage.cs
@@ -0,0 +1,22 @@
+﻿namespace ByndyusoftTest.Support.PageObjects
+{
+    public class GoogleMainPage:AbstractPage
+    {
+        public IWebElement TextBoxSearch => _driver.FindElement(By.XPath("//input[@title='Поиск']"));
+        public IWebElement ButtonSearchInGoogle => _driver.FindElement(By.XPath("//input[@title='Поиск']"));
+
+        public GoogleMainPage(string driver) : base(driver)
+        {
+        }
+
+        public void InputSearchText(string text)
+        {
+            TextBoxSearch.SendKeys(text);
+        }
+
+        public void ClickButtonSearchInGoogle()
+        {
+            ButtonSearchInGoogle.Click();
+        }
+    }
+}
---
+++ ByndyusoftTest/Support/PageObjects/GoogleSearchPage.cs
@@ -0,0 +1,16 @@
+﻿namespace ByndyusoftTest.Support.PageObjects
+{
+    public class GoogleSearchPage : AbstractPage
+    {
+        public IWebElement FirstSearchResult => _driver.FindElement(By.XPath("//h1[text()='Результаты поиска']/following-sibling::div/div[1]//link"));
+
+        public GoogleSearchPage(string driver) : base(driver)
+        {
+        }
+
+        public void ClickFirstSearchResult()
+        {
+            FirstSearchResult.Click();
+        }
+    }
+}
