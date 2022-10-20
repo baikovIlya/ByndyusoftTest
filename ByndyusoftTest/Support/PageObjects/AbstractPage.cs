f8b9ac20264744b9afe51fb51dc6ab6f1d6e7574
baikovIlya <baikusha23@gmail.com>
2022-10-20 23:36:22 +0300

    add: AbstractPage

---
+++ ByndyusoftTest/Support/PageObjects/AbstractPage.cs
@@ -0,0 +1,13 @@
+﻿namespace ByndyusoftTest.Support.PageObjects
+{
+    public abstract class AbstractPage
+    {
+        protected IWebDriver _driver;
+
+        public AbstractPage(string driver)
+        {
+            _driver = Singleton.GetDriverInstance(driver);
+            _driver.Manage().Window.Maximize();
+        }
+    }
+}
