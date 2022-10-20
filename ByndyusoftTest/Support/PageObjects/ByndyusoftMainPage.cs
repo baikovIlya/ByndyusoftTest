0b682f4a339896e7bfae15abe90040a27e000e1c
baikovIlya <baikusha23@gmail.com>
2022-10-21 00:04:05 +0300

    add: ByndysoftMainPage

---
+++ ByndyusoftTest/Support/PageObjects/ByndyusoftMainPage.cs
@@ -0,0 +1,18 @@
+﻿namespace ByndyusoftTest.Support.PageObjects
+{
+    public class ByndyusoftMainPage : AbstractPage
+    {
+        public IWebElement ButtonOrderPresentation => _driver.FindElement(By.XPath("//*[text()='Узнайте больше о наших продуктах']/following-sibling::*[contains(text(),'Заказать презентацию')]"));
+        public IWebElement LinkMailInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='mailto:sales@byndyusoft.com']"));
+        public IWebElement LinkPhoneInPopUpWindow => _driver.FindElement(By.XPath("//div[@class='popup-callback__content']//a[@href='tel:88007751521']"));
+
+        public ByndyusoftMainPage(string driver) : base(driver)
+        {
+        }
+
+        public void ClickButtonOrderPresentation()
+        {
+            ButtonOrderPresentation.Click();
+        }
+    }
+}
