61c507ceb2a8925988276b306b44ea4123b8e4d3
baikovIlya <baikusha23@gmail.com>
2022-10-21 00:34:54 +0300

    add: step definitions

---
+++ ByndyusoftTest/StepDefinitions/PopUpWindowOrderPresentationStepDefinitions.cs
@@ -0,0 +1,54 @@
+namespace ByndyusoftTest.StepDefinitions
+{
+    [Binding]
+    public sealed class PopUpWindowOrderPresentationStepDefinitions
+    {
+        [Given(@"Open Google web page")]
+        public void GivenOpenGoogleWebPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Input ""([^""]*)"" in textbox search")]
+        public void WhenInputInTextboxSearch(string byndysoft)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Click button Search In Google")]
+        public void WhenClickButtonSearchInGoogle()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Click on first link")]
+        public void WhenClickOnFirstLink()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"Click on button OrderPresentation")]
+        public void WhenClickOnButtonOrderPresentation()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"PopUp window should appear")]
+        public void ThenPopUpWindowShouldAppear()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Email in PopUp window should match ""([^""]*)""")]
+        public void ThenEmailInPopUpWindowShouldMatch(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"Phone number in PopUp window should match ""([^""]*)""")]
+        public void ThenPhoneNumberInPopUpWindowShouldMatch(string p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
