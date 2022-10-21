77e1a263addd163c2d7c79ec71d354c0e1f41821
baikovIlya <baikusha23@gmail.com>
2022-10-21 13:57:14 +0300

    add: waitings for button Search In Google

--- ByndyusoftTest/Support/PageObjects/GoogleMainPage.cs
+++ ByndyusoftTest/Support/PageObjects/GoogleMainPage.cs
@@ -4,7 +4,7 @@
     {
         private const string PageUrl = "https://www.google.ru/";
         public IWebElement TextBoxSearch => _driver.FindElement(By.XPath("//input[@title='Поиск']"));
-        public IWebElement ButtonSearchInGoogle => _driver.FindElement(By.XPath("//div[@class='lJ9FBc']//input[@value='Поиск в Google']"));
+        public IWebElement ButtonSearchInGoogle => GetButtonSearchInGoogle();
 
         public GoogleMainPage() : base()
         {
@@ -23,6 +23,12 @@
         public void OpenThisPage()
         {
             _driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        private IWebElement GetButtonSearchInGoogle()
+        {
+            WebDriverWait webDriverWait = new WebDriverWait(_driver, TimeSpan.FromSeconds(1));
+            return webDriverWait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='lJ9FBc']//input[@value='Поиск в Google']")));
         }
     }
 }
